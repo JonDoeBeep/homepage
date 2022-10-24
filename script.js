40e3c702330f5393d6fce2bef3eb33999921420b
Jon Doe <56412184+JonDoeBeep@users.noreply.github.com>
2022-10-24 09:24:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Alternative Versions:
+// - JS ScrollTimeline 2022: https://codepen.io/bramus/pen/MWVExQg
+// - CSS view-timeline 2022: https://codepen.io/bramus/pen/GRdGoKy/ 👈 = The version you are currently looking at
+// - JS Motion One: https://codepen.io/bramus/pen/PoRKEaZ 
+// - CSS @scroll-timeline 2021 (Old Syntax): https://codepen.io/bramus/pen/xxRZZdK
+// - JS ScrollTimeline 2021 (Old Syntax): https://codepen.io/bramus/pen/PobZbBV
